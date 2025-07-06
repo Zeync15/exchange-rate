8e22abc93c291a7069a135da26c2451979bac8c4
Ze Yen <zeyenchai@gmail.com>
2025-07-07 00:56:56 +0800

    handle empty records, show records amount

--- src/component/Table.tsx
+++ src/component/Table.tsx
@@ -48,29 +48,35 @@ export const Table = <TData,>({ data, columns }: TableProps<TData>) => {
           ))}
         </thead>
         <tbody>
-          {table.getRowModel().rows.map((row) => (
-            <tr key={row.id} className="even:bg-gray-50">
-              {row.getVisibleCells().map((cell) => (
-                <td
-                  key={cell.id}
-                  className="px-4 py-2 border-b border-gray-200"
-                >
-                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                </td>
-              ))}
+          {table.getRowModel().rows.length > 0 ? (
+            table.getRowModel().rows.map((row) => (
+              <tr key={row.id} className="even:bg-gray-50">
+                {row.getVisibleCells().map((cell) => (
+                  <td
+                    key={cell.id}
+                    className="px-4 py-2 border-b border-gray-200"
+                  >
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </td>
+                ))}
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td
+                colSpan={columns.length}
+                className="px-4 py-6 text-center text-gray-500"
+              >
+                No records found
+              </td>
             </tr>
-          ))}
+          )}
         </tbody>
       </table>
 
       <div className="flex items-center gap-2 p-2 justify-between">
         <div className="">
-          {/* <span className="flex items-center gap-1">
-            <div>
-              {table.getState().pagination.pageIndex + 1} -{" "}
-              {table.getRowCount()} records
-            </div>
-          </span> */}
+          <div className="px-2">{table.getRowCount()} records</div>
         </div>
         <div className="flex gap-5 items-center">
           <button
